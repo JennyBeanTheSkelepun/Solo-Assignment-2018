9132efbf230560a1b8a60cbcf7b99da70bf5ac05
Jennifer Field <43635934+JennyBeanTheSkelepun@users.noreply.github.com>
2018-10-04 10:41:10 +0100

    Initial uploading

---
+++ Source/SoloProjectTGP/SoloProjectTGP.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SoloProjectTGP.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SoloProjectTGP, "SoloProjectTGP" );
